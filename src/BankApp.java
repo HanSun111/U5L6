87b89bbdc35fee974868f70daa55b52d0c23a15a
hansun <hsun7920@bths.edu>
2022-12-11 22:17:15 -0500

    bro, cannot for the life of me figure out the challenge

--- src/BankApp.java
+++ src/BankApp.java
@@ -1,10 +1,19 @@
 public class BankApp {
-    private Bank laScam;
-    private CreditCard gimmeMoney;
+    private String name;
     private BagelShop bunsWithHoles;
 
-    public String user(String name){
-        if
+    public BankApp(String name, String shop, int inventory, int price, String cardPin){
+        this.name = name;
+        Bank laScam = new Bank();
+        bunsWithHoles = new BagelShop(shop, inventory, price, laScam);
+        CreditCard firstCard = new CreditCard(this.name, cardPin);
+    }
+    public void pays(CreditCard cards, int quantity, String cardPin){
+        bunsWithHoles.payForBagels(cards, quantity, cardPin);
+    }
+
+    public void returns(CreditCard cards, int quantity, String cardPin){
+        bunsWithHoles.returnBagels(cards, quantity, cardPin);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,109 +1,42 @@
+import java.util.Scanner;
 public class Main
 {
-    public static void main(String[] args)
-    {
-        //New bank in town
-        Bank bank = new Bank();
-        System.out.println(bank + "\n");
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("input a name: ");
+        String name = s.nextLine();
+        System.out.println("input a name for bagel shop: ");
+        String bagelName = s.nextLine();
+        System.out.println("input the inventory for bagel shop: ");
+        int amount = s.nextInt();
+        System.out.println("input price you charge per bagel: ");
+        int price = s.nextInt();
+        System.out.println("input a pin for new credit card");
+        String password = s.nextLine();
 
-        //Mr. Das is getting a new credit card!
-        CreditCard myCard = new CreditCard("Mr. Das", "9254");
-        System.out.println(myCard + "\n");
+        BankApp app = new BankApp(name, bagelName, amount, price, password);
+        System.out.println("press 1 to see menu, 2 to stop");
+        int choice = s.nextInt();
+        for(int i = 0; choice != 2; i++){
+            System.out.println("MENU\n1. Purchase Bagel\n2. Return bagel\n3. Open Second Card or choose card to pay");
+            int moreChoices = s.nextInt();
+            if(moreChoices == 1){
+                if(i == 0){
 
-        //Oh nice! A new bagel shop in town!
-        BagelShop jimsBagelShop = new BagelShop("Jim's Bagel Shop", 1000, 2, bank);
-        System.out.println(jimsBagelShop + "\n");
+                }
+                app.pays();
+            }
+            if(moreChoices == 2){
 
-        //Mr. Das wants to buy 5 bagels using his new credit card
-        boolean buyBagels = jimsBagelShop.payForBagels(myCard, 5, "9254");
-        System.out.println(buyBagels + "\n");
-        System.out.println(myCard + "\n");
-        System.out.println(jimsBagelShop + "\n");
+            }
+            if(moreChoices == 3){
+                for(int j = 0; j==1; j++) {
+                    System.out.println("Enter a pin");
+                    String pin = s.nextLine();
+                    app.openNewCard(pin);
+                }
 
-        //He tried to buy 10 more, but accidentally entered his PIN wrong
-        buyBagels = jimsBagelShop.payForBagels(myCard, 10, "9354");
-        System.out.println(buyBagels + "\n");
-        System.out.println(myCard + "\n");
-        System.out.println(jimsBagelShop + "\n");
-
-        //He fixed his typo!  Phew thank goodness
-        buyBagels = jimsBagelShop.payForBagels(myCard, 10, "9254");
-        System.out.println(buyBagels + "\n");
-        System.out.println(myCard + "\n");
-        System.out.println(jimsBagelShop + "\n");
-
-        //Three of his bagels are moldy; he returns them for his money back
-        boolean returnBagels = jimsBagelShop.returnBagels(myCard, 3, "9254");
-        System.out.println(returnBagels + "\n");
-        System.out.println(myCard + "\n");
-        System.out.println(jimsBagelShop + "\n");
-
-        //Bill time!  Mr. Das pays off $15
-        bank.makePayment(myCard, 15);
-        System.out.println(myCard + "\n");
-        System.out.println(bank + "\n");
-
-        //John opens a new Credit Card
-        CreditCard johnsCard = new CreditCard("John", "1022");
-        System.out.println(johnsCard + "\n");
-
-        //John is catering a large breakfast at a conference center
-        buyBagels = jimsBagelShop.payForBagels(johnsCard, 300, "1022");
-        System.out.println(buyBagels + "\n");
-        System.out.println(johnsCard + "\n");
-        System.out.println(jimsBagelShop + "\n");
-
-        //Yikes! That was expensive. John pays off $75 of his credit card balance
-        bank.makePayment(johnsCard, 75);
-        System.out.println(johnsCard + "\n");
-        System.out.println(bank + "\n");
-
-        //The bagel shop has a lot of money and wants to deposit the profits into the bank
-        jimsBagelShop.depositProfits();
-        System.out.println(jimsBagelShop + "\n");
-        System.out.println(bank + "\n");
-
-        //Let's see whose credit card balance is lower!
-        CreditCard lowestCard = bank.lowerBalance(myCard, johnsCard);
-        if (lowestCard != null)
-        {
-            System.out.println(lowestCard.getAccountHolder() + " with a $" + lowestCard.getBalanceOwed() + " balance\n");
-        }
-        else
-        {
-            System.out.println("They have the same balance\n");
-        }
-
-        //John pays off his card in full
-        bank.makePayment(johnsCard, johnsCard.getBalanceOwed());
-        System.out.println(johnsCard + "\n");
-        System.out.println(bank + "\n");
-
-        //Now let's check again:
-        lowestCard = bank.lowerBalance(myCard, johnsCard);
-        if (lowestCard != null)
-        {
-            System.out.println(lowestCard.getAccountHolder() + " with a $" + lowestCard.getBalanceOwed() + " balance\n");
-        }
-        else
-        {
-            System.out.println("They have the same balance\n");
-        }
-
-        //Mr. Das pays off his card in full
-        bank.makePayment(myCard, myCard.getBalanceOwed());
-        System.out.println(myCard + "\n");
-        System.out.println(bank + "\n");
-
-        //Now let's check one last time:
-        lowestCard = bank.lowerBalance(myCard, johnsCard);
-        if (lowestCard != null)
-        {
-            System.out.println(lowestCard.getAccountHolder() + " with a $" + lowestCard.getBalanceOwed() + " balance\n");
-        }
-        else
-        {
-            System.out.println("They have the same balance\n");
+            }
         }
     }
 }
