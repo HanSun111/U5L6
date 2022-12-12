85a39bcb42e63d4836e4fdfcd9379a4a30d43ca8
hansun <hsun7920@bths.edu>
2022-12-11 22:55:25 -0500

    bro, cannot for the life of me figure out the challenge

--- src/BankApp.java
+++ src/BankApp.java
@@ -1,19 +1,49 @@
+import java.util.concurrent.Callable;
+
 public class BankApp {
     private String name;
+    private Bank laScam;
     private BagelShop bunsWithHoles;
+    private CreditCard firstCard;
+    private String password;
 
     public BankApp(String name, String shop, int inventory, int price, String cardPin){
         this.name = name;
-        Bank laScam = new Bank();
+        laScam = new Bank();
         bunsWithHoles = new BagelShop(shop, inventory, price, laScam);
-        CreditCard firstCard = new CreditCard(this.name, cardPin);
-    }
-    public void pays(CreditCard cards, int quantity, String cardPin){
-        bunsWithHoles.payForBagels(cards, quantity, cardPin);
+        firstCard = new CreditCard(this.name, cardPin);
     }
 
-    public void returns(CreditCard cards, int quantity, String cardPin){
-        bunsWithHoles.returnBagels(cards, quantity, cardPin);
+    public void choices(int imDyingItsLateHairLineRetreating) {
+        int condition = 15;
+        while (condition != 15) {
+            CreditCard second = null;
+            if (imDyingItsLateHairLineRetreating == 1) {
+                while(second == null) {
+                    bunsWithHoles.payForBagels(firstCard, );
+                }
+            }
+            if (imDyingItsLateHairLineRetreating == 2) {
+                while(second == null) {
+                    bunsWithHoles.returnBagels(firstCard, )
+                }
+            }
+            if (imDyingItsLateHairLineRetreating == 3) {
+
+            }
+            if (imDyingItsLateHairLineRetreating == 4) {
+                for (int i = 0; i != 1; i++) {
+                    second = new CreditCard(name, password);
+                }
+
+            }
+            if (imDyingItsLateHairLineRetreating == 5) {
+                laScam.lowerBalance(firstCard, second);
+            }
+            if (imDyingItsLateHairLineRetreating == 6) {
+                condition = 15;
+            }
+        }
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -20,21 +20,6 @@ public class Main
         for(int i = 0; choice != 2; i++){
             System.out.println("MENU\n1. Purchase Bagel\n2. Return bagel\n3. Open Second Card or choose card to pay");
             int moreChoices = s.nextInt();
-            if(moreChoices == 1){
-                if(i == 0){
-
-                }
-                app.pays();
-            }
-            if(moreChoices == 2){
-
-            }
-            if(moreChoices == 3){
-                for(int j = 0; j==1; j++) {
-                    System.out.println("Enter a pin");
-                    String pin = s.nextLine();
-                    app.openNewCard(pin);
-                }
 
             }
         }
